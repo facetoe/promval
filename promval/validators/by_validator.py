09c77b5aa15e21d1727767d22af59766b2b91948
William Coe <willcoe@aiven.io>
2022-01-23 05:46:20 -0500

    validators,tests: add ByLabelValueValidator

--- promval/validators/by_validator.py
+++ promval/validators/by_validator.py
@@ -12,9 +12,35 @@ class ByGroupValidator(Validator):
     def enterBy(self, ctx: PromQLParser.ByContext):
         groupings = set()
         for label in ctx.labelNameList().labelName():
-            groupings.add(str(label.METRIC_NAME()))
+            groupings.add(str(label.METRIC_NAME().getText()))
 
         missing = self.expected.difference(groupings)
         if missing:
             message = f"missing required metric names {missing}"
             self.errors.append(Error(ctx, message))
+
+
+class ByLabelValueValidator(Validator):
+    def __init__(self, label_value: str):
+        self.target_label_value = label_value
+        self.stack = []
+
+    def enterLabelMatcherList(self, ctx: PromQLParser.LabelMatcherListContext):
+        for label in ctx.labelMatcher():
+            label_name = label.labelName().METRIC_NAME().getText()
+            label_value = label.STRING().getText()
+            label_value = label_value[1:-1]  # strip quotes
+            if label_value == self.target_label_value:
+                by_labels, by_ctx = self.stack[-1]
+                if label_name not in by_labels:
+                    message = f"expected label name '{label_name}' in by clause"
+                    self.errors.append(Error(by_ctx, message=message))
+
+    def exitLabelMatcherList(self, ctx: PromQLParser.LabelMatcherListContext):
+        self.stack.pop()
+
+    def enterBy(self, ctx: PromQLParser.ByContext):
+        by_labels = set()
+        for label in ctx.labelNameList().labelName():
+            by_labels.add(str(label.METRIC_NAME()))
+        self.stack.append((by_labels, ctx))
--- tests/validators/test_by_validator.py
+++ tests/validators/test_by_validator.py
@@ -1,10 +1,10 @@
 import pytest
 
 from promval.error import ValidationError
-from promval.validators.by_validator import ByGroupValidator
+from promval.validators.by_validator import ByGroupValidator, ByLabelValueValidator
 
 
-def test_validator_valid():
+def test_group_validator_valid():
     expr = """
         sum by (job) (
           rate(http_requests_total[5m])
@@ -14,12 +14,29 @@ def test_validator_valid():
     validator.validate(expr)
 
 
-def test_validator_missing_group():
+def test_group_validator_missing_group():
     expr = """
         sum by (job) (
           rate(http_requests_total[5m])
         )
     """
     validator = ByGroupValidator(expected={"job", "thing"})
+    with pytest.raises(ValidationError):
+        validator.validate(expr)
+
+
+def test_label_validator_valid():
+    expr = """
+        max by (very, second)(foo_metric{very=~'important', something='else'})
+    """
+    validator = ByLabelValueValidator(label_value="important")
+    validator.validate(expr)
+
+
+def test_label_validator_missing_label():
+    expr = """
+        max by (ve, second)(foo_metric{very=~'important', something='else'})
+    """
+    validator = ByLabelValueValidator(label_value="important")
     with pytest.raises(ValidationError):
         validator.validate(expr)
