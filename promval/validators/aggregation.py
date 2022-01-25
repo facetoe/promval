2d352f101dcd83f1e4f03c2ad5287b7dea4c7e12
William Coe <willcoe@aiven.io>
2022-01-24 21:37:38 -0500

    validators: support validating multiple label clauses
    Operate on the level of aggregation so we can correctly handle multiple
    label statements.

--- promval/validators/aggregation.py
+++ promval/validators/aggregation.py
@@ -20,7 +20,7 @@ class ByGroupValidator(Validator):
             self.errors.append(Error(ctx, message))
 
 
-class ByLabelValueValidator(Validator):
+class AggregationLabelValueValidator(Validator):
     def __init__(self, label_value: str):
         self.target_label_value = label_value
         self.stack = []
@@ -31,16 +31,24 @@ class ByLabelValueValidator(Validator):
             label_value = label.STRING().getText()
             label_value = label_value[1:-1]  # strip quotes
             if label_value == self.target_label_value:
-                by_labels, by_ctx = self.stack[-1]
-                if label_name not in by_labels:
-                    message = f"expected label name '{label_name}' in by clause"
-                    self.errors.append(Error(by_ctx, message=message))
+                labels, context = self.stack[-1]
+                if label_name not in labels:
+                    message = (
+                        f"expected label name '{label_name}' in aggregation clause"
+                    )
+                    self.errors.append(Error(context, message=message))
 
-    def exitLabelMatcherList(self, ctx: PromQLParser.LabelMatcherListContext):
-        self.stack.pop()
+    def enterAggregation(self, ctx: PromQLParser.AggregationContext):
+        by, without = ctx.by(), ctx.without()
+        if by:
+            context = by
+            label_list = by.labelNameList()
+        else:
+            context = without
+            label_list = without.labelNameList()
 
-    def enterBy(self, ctx: PromQLParser.ByContext):
-        by_labels = set()
-        for label in ctx.labelNameList().labelName():
-            by_labels.add(str(label.METRIC_NAME()))
-        self.stack.append((by_labels, ctx))
+        labels = []
+        for label in label_list.labelName():
+            label_name = label.getText()
+            labels.append(label_name)
+        self.stack.append((labels, context))
--- tests/validators/test_by_validator.py
+++ tests/validators/test_by_validator.py
@@ -1,7 +1,7 @@
 import pytest
 
 from promval.error import ValidationError
-from promval.validators.by_validator import ByGroupValidator, ByLabelValueValidator
+from promval.validators.aggregation import ByGroupValidator, AggregationLabelValueValidator
 
 
 def test_group_validator_valid():
@@ -42,7 +42,7 @@ def test_label_validator_valid():
     expr = """
         max by (very, second)(foo_metric{very=~'important', something='else'})
     """
-    validator = ByLabelValueValidator(label_value="important")
+    validator = AggregationLabelValueValidator(label_value="important")
     validator.validate(expr)
 
 
@@ -50,6 +50,15 @@ def test_label_validator_missing_label():
     expr = """
         max by (ve, second)(foo_metric{very=~'important', something='else'})
     """
-    validator = ByLabelValueValidator(label_value="important")
+    validator = AggregationLabelValueValidator(label_value="important")
+    with pytest.raises(ValidationError):
+        validator.validate(expr)
+
+
+def test_label_validator_multiple_labels():
+    expr = """
+        avg by (first, second)(foo_metric{very=~'important'} / foo_metric{very=~'important'}) > 8
+    """
+    validator = AggregationLabelValueValidator(label_value="important")
     with pytest.raises(ValidationError):
         validator.validate(expr)
