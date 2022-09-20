1de18c2b24d997691fdd3d193e0223e132ee4e02
William Coe <willcoe@aiven.io>
2022-09-20 10:06:37 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="promval",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
-        "antlr4-python3-runtime>=4.9.3"
+        "antlr4-python3-runtime>=4.10"
     ],
     tests_require=["pytest"],
     setup_requires=["flake8", "black"],
