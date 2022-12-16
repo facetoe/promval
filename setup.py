f0c3e1df8678feb81fd22709224da0e45ef61e73
Adam Martin <89938352+aiven-amartin@users.noreply.github.com>
2022-12-16 17:31:51 -0500

    Update setup.py
    Don't want to install a tests module, as it conflicts with other installs and doesn't provide any needed packages.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="promval",
     version="0.0.3",
-    packages=find_packages(),
+    packages=find_packages(exclude="tests"),
     install_requires=[
         "antlr4-python3-runtime>=4.10"
     ],
