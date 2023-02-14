7a2c8bb8e4ccb17bc5dce84be49fa0f2c85c70cd
Mario Apra <mario.apra@aiven.io>
2023-02-14 17:57:54 +1300

    Update version
    The idea is for the next release to include https://github.com/facetoe/promval/pull/9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="promval",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "antlr4-python3-runtime>=4.10"
